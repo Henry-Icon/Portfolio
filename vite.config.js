a6ce61e6abda7e63e6c96cc8cf566c3cce965ec7
henry-icon <henryfilani097@gmail.com>
2025-07-12 18:41:57 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/' // 👈 Must match your repo name exactly!
-})
+  base: '/Frontend-Dev-Portfolio/' // make sure this matches your GitHub repo name
+});
