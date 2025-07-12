768bd32dcd4117e9f3b12b56a899f64a6d5b5852
henry-icon <henryfilani097@gmail.com>
2025-07-12 12:20:03 +0100

    Set base path in Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Protfoilo/'  // MUST match your repo name exactly, including spelling!
+  base: '/Portfolio/' // 👈 Must match your repo name exactly!
 })
