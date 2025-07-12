3654c5345a8e6a632fc293a69a6f67ea5bcbfd22
henry-icon <henryfilani097@gmail.com>
2025-07-12 19:17:26 +0100

    Added base config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/Frontend-Dev-Portfolio/',
   plugins: [react()],
-  base: '/Frontend-Dev-Portfolio/', // Must match the repo name exactly
-})
+});
